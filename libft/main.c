c60cea71b6e609ad1337f596982d78a419f0d3e4
Dahyeong Kang <trumpjqk@daum.net>
2022-07-07 20:11:39 +0900

    220707

--- libft/main.c
+++ libft/main.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include "srcs/ft_memset.c"
 #include "srcs/ft_strlen.c"
+#include "srcs/ft_atoi.c"
 
 
 int	main()
@@ -19,11 +20,22 @@ int	main()
 	puts(str);
 	printf(">===============<\n\n\n");
 
+	//strlen
 	printf(">----strlen-----<\n");
 	printf("%zu ", ft_strlen("hello"));
 	printf("%zu ", ft_strlen(""));
 	printf("%zu \n", ft_strlen("a\n"));
 	printf(">===============<\n\n\n");
 
+	//atoi
+	printf(">----atoi-----<\n");
+	//char	*ptr = NULL;
+
+	printf("123: %d\n", ft_atoi("  123"));
+	printf("2147483647: %d\n", ft_atoi("2147383647"));
+	printf("-2147384648: %d\n", ft_atoi("-2147383648"));
+	printf("0: %d\n", ft_atoi("0"));
+	//printf("NULL ptr: %d\n", ft_atoi(ptr));
+	printf(">===============<\n\n\n");
 	return 0;
 }
--- libft/srcs/ft_atoi.c
+++ libft/srcs/ft_atoi.c
@@ -1,15 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dahkang <dahkang@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/07 20:11:33 by dahkang           #+#    #+#             */
+/*   Updated: 2022/07/07 20:11:34 by dahkang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	is_space(char ch)
 {
 	if (ch == '\t' || ch == '\n' || ch == '\v'
 		|| ch == '\f' || ch == '\r' || ch == ' ')
-		return (1);
-	else
-		return (0);
-}
-
-int	is_sign(char ch)
-{
-	if (ch == '+' || ch == '-')
 		return (1);
 	else
 		return (0);
@@ -23,6 +27,29 @@ int	is_number(char ch)
 		return (0);
 }
 
-//NULL_PTR segmentation fault 
+//When a null pointer is passed as a parameter, a seg fault occurs.
+//Omitting errno related matters
 int	ft_atoi(const char *str)
 {
+	int	i;
+	int	sign;
+	int	ret;
+
+	i = 0;
+	sign = 1;
+	ret = 0;
+	while (is_space(str[i]))
+		i++;
+	if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	while (is_number(str[i]))
+	{
+		ret *= 10;
+		ret += sign * (str[i] - '0');
+		i++;
+	}
+	return (ret);
+}
