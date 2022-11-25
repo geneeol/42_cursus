e111cc096e35a49723e06dcace69e427c5610356
Dahyeong Kang <trumpjqk@daum.net>
2022-11-25 20:48:58 +0900

    tiny

--- push_swap/parser.c
+++ push_swap/parser.c
@@ -6,7 +6,7 @@
 /*   By: dahkang <dahkang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 17:51:51 by dahkang           #+#    #+#             */
-/*   Updated: 2022/11/25 19:10:23 by dahkang          ###   ########.fr       */
+/*   Updated: 2022/11/25 20:48:38 by dahkang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,4 @@ t_info	*input_parser(int argc, char **argv)
 	if (argc < 2)
 		return (0);
 	if (argc == 2)
-		ret = par 
+		ret = parse_split() 
