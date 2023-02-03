db081f9dfd9f7b0731c82c0100956790ba37cecb
geneeol <trumpjqk@daum.net>
2023-02-03 21:39:58 +0900

    Test: testcase 변경

--- philosophers/test/gettimeofday.c
+++ philosophers/test/gettimeofday.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-int	get_cur_time(void)
+unsigned long long	get_cur_time(void)
 {
-	struct timeval	tp;
-	int				cur_time;
+	struct timeval		tp;
+	unsigned long long	cur_time;
 
 	gettimeofday(&tp, NULL);
+	printf("tv_sec: %ld\n", tp.tv_sec);
+	printf("tv_usec: %d\n", tp.tv_usec);
 	cur_time = tp.tv_sec * 1000000 + tp.tv_usec;
 	return (cur_time);
 }
 
 int	main(void)
 {
-	printf("ret: %d\n", get_cur_time());
+	printf("ret: %lld\n", get_cur_time());
 }
